9f4ed9ec99636d3b510fbff7bed6126807c43c00
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2021-03-09 18:30:41 -0300

    update 0.0.3
    Added a SceneHandler, which will change scene from one to another.  Also created new Scene "Splash". The buildIndex for the "App" menu, is 0. The other is 1.

---
+++ Assets/_Scripts/SceneHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public Image fadeOutPanel;
+    AudioSource audioSourceComponent;
+    public AudioClip buttonAudioClip;
+
+    private void Awake()
+    {
+        audioSourceComponent = GetComponent<AudioSource>();
+    }
+    public void ChangeScene(int scene)
+    {
+        audioSourceComponent.PlayOneShot(buttonAudioClip);
+        StartCoroutine(ChangingScene(0));
+    }
+    IEnumerator ChangingScene(int scene)
+    {
+        Color c = fadeOutPanel.color;
+        c.a = 1;
+        Tween toFade = fadeOutPanel.DOColor(c,2);
+        yield return toFade.WaitForCompletion();
+        SceneManager.LoadScene(scene);
+
+    }
+}
